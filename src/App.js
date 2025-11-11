f9952a824395c91e6b9190d1ed279209f405d6a6
pxy05 <psky0695@gmail.com>
2025-11-11 18:17:35 +0000

    some setup stuff - tailwindcss - shadcn

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to My Personal Site</h1>
-        <p>This is a basic React site.</p>
+    <div className="App master">
+      <header>
+        <div className="fade-bottom min-h-[5vh] header"/>
       </header>
+        <div className="grid grid-cols-3">
+          <div/>
+          <div className="flex flex-col justify-center items-center gap-4 text-white text-4xl font-bold mt-[5vh]">
+            <h1 className="">Hi, I'm Praj Yakha</h1>
+            <p className="">I'm a software engineer</p>
+          </div>
+          <div/>
+      </div>
+      <footer>
+        <div className="fade-top min-h-[5vh] footer"/>
+      </footer>
     </div>
   );
 }
